d9f0e19631520887ca62ead83883a2aac3386da7
azeez <oazeez112@gmail.com>
2020-03-01 23:09:42 +0000

    add a pop up error message

--- javascript.js
+++ javascript.js
@@ -4,3 +4,14 @@ const emailInput = document.querySelector('#email');
 const message = document.querySelector('#msg');
 const usersList = document.querySelector('#users')
 const button = document.querySelector('#btn');
+
+form.addEventListener('submit', onsubmit);
+function onsubmit(e) {
+    e.preventDefault();
+    if (nameInput.value == "" || emailInput.value == "") {
+        message.classList.add('error')
+        message.innerHTML = '<p>Please Enter All Fields</p>'
+        setTimeout(() => message.remove(), 2000)
+    }
+
+}
