6db3e89d31836d90fd75c77fa3d085aca832db9e
azeez <oazeez112@gmail.com>
2020-03-01 22:56:01 +0000

    cached(selected the dom element) the dom

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('#form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const message = document.querySelector('#msg');
+const usersList = document.querySelector('#users')
+const button = document.querySelector('#btn');
